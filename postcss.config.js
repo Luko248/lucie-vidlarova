3f022c5dc45a428990098db0f834c3e9ea3e6db4
Lukáš Chylík <lukas.chylik@riganti.cz>
2021-05-04 07:07:02 +0200

    df

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@
     map: context.options.map,
     parser: context.options.parser,
     plugins: [
-        require('autoprefixer')({ browsers: "last 3 versions", grid: true })
+        require('autoprefixer')({
+            grid: true,
+            flex: true
+        })
     ]
 })
