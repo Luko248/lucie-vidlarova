7184f5f0abd21b204303534075c256f952f1daa2
Lukáš Chylík <lukas.chylik@riganti.cz>
2022-01-14 12:10:10 +0100

    nav updated

--- Resources/Scripts/navigation.ts
+++ Resources/Scripts/navigation.ts
@@ -1,11 +1,22 @@
 const menuBtn = document.getElementById('nebuBtn');
 const menu = document.querySelector('.nav__menu');
+const navItems = document.querySelectorAll('.nav__item');
 const htmlDoc = document.querySelector('body');
+
+function toggleNavClasses(){
+    menuBtn.classList.toggle('burger--open');
+    menu.classList.toggle('nav__menu--open');
+    htmlDoc.classList.toggle('overflow-hidden');
+}
 
 export function initNav(){
     menuBtn.addEventListener('click', () =>{
-        menuBtn.classList.toggle('burger--open');
-        menu.classList.toggle('nav__menu--open');
-        htmlDoc.classList.toggle('overflow-hidden');
+        toggleNavClasses();
+    });
+
+    navItems.forEach(navItems =>{
+        navItems.addEventListener('click', () =>{
+            toggleNavClasses();
+        });
     });
 }
--- wwwroot/js/init.js
+++ wwwroot/js/init.js
@@ -13,12 +13,21 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.initNav = void 0;
 const menuBtn = document.getElementById('nebuBtn');
 const menu = document.querySelector('.nav__menu');
+const navItems = document.querySelectorAll('.nav__item');
 const htmlDoc = document.querySelector('body');
+function toggleNavClasses() {
+    menuBtn.classList.toggle('burger--open');
+    menu.classList.toggle('nav__menu--open');
+    htmlDoc.classList.toggle('overflow-hidden');
+}
 function initNav() {
     menuBtn.addEventListener('click', () => {
-        menuBtn.classList.toggle('burger--open');
-        menu.classList.toggle('nav__menu--open');
-        htmlDoc.classList.toggle('overflow-hidden');
+        toggleNavClasses();
+    });
+    navItems.forEach(navItems => {
+        navItems.addEventListener('click', () => {
+            toggleNavClasses();
+        });
     });
 }
 exports.initNav = initNav;
